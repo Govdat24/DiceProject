784e1f8375f2cccc841cc2c9c51d065a1db05c13
govher <govhertm@gmail.com>
2024-09-20 10:55:55 +0200

    Govher

--- src/opgave01/RollTwoDice.java
+++ src/opgave01/RollTwoDice.java
@@ -1,4 +1,63 @@
 package opgave01;
 
 public class RollTwoDice {
+
+    public static int rollCount = 0;
+    public static int addedValue = 0;
+    public static int countSame = 0;
+
+    public static void main(String[] args) {
+
+        printRules();
+        for (int rolls = 0; rolls < 6; rolls++) {
+            int[] rollDiceResult = rollDice();
+            System.out.println(rolls + 1 + " kast " + rollDiceResult[0] + ", " + rollDiceResult[1]);
+        }
+        System.out.println("Du har slået " + rollCount + " gange ");
+        System.out.println(" Den samlede værdi af dine kast er " + addedValue);
+        System.out.println("Du har slået " + countSame + " ens ");
+    }
+
+    private static void printRules() {
+        System.out.println("=====================================================");
+        System.out.println("Kast to terning 6 gange");
+        System.out.println("Spilleren ruller to terning, så længde man lyster.");
+        System.out.println("=====================================================");
+
+    }
+
+    public static int[] rollDice() {
+        int[] rollArray = new int[2];
+
+        rollArray[0] = (int) (Math.random() * 6 + 1); //6 sider
+        rollArray[1] = (int) (Math.random() * 6 + 1);
+        adddValue(rollArray);
+        updateRollCount();
+        return rollArray;
+    }
+
+
+    public static void adddValue(int[] addFaces) {
+        addedValue += addFaces[0] + addFaces[1];
+    }
+
+
+    public static void updateRollCount() {
+        rollCount++;
+    }
+
+    public static boolean sameValue(int[] sameFaces) {
+
+        if (sameFaces[0] == sameFaces[1]) {
+            countSame = 0;
+        }
+        return false;
+    }
+
+
+    private static void printStatistics() {
+        System.out.println("\nResults:");
+        System.out.println("-------");
+        System.out.printf("%17s %4d\n", "Antal rul:", rollCount);
+    }
 }
--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -1,4 +1,74 @@
 package opgave02;
 
 public class Craps {
+    public static void main(String[] args) {
+        playCraps();
+    }
+
+    public static void playCraps() {
+//        int firstDice = (int) (Math.random() * 6 + 1); //6 sides
+//        int secondDice = (int) (Math.random() * 6 + 1);
+//        int firstRoll = firstDice + secondDice;
+        int firstRoll = rollDice();
+
+        if (firstRoll == 7 || firstRoll == 11) {
+            System.out.println("You rolled " + firstRoll + " and so you win");
+        } else if (firstRoll == 2 || firstRoll == 3 || firstRoll == 12) {
+            System.out.println("You rolled " + firstRoll + " and so you lose");
+        } else {
+            rollforPoint(firstRoll);
+
+//            System.out.println("You rolled " + firstRoll + " and so game continues");
+//            int point = firstRoll;
+//            if (rollforPoint(point)) {
+//                System.out.println("You rolled your point " + point + " and so you win");
+//            } else {
+//                System.out.println("You rolled 7 and so you lose");
+        }
+    }
+
+    public static int rollDice() {
+        int roll1 = (int) (Math.random() * 6 + 1); //6 sides
+        int roll2 = (int) (Math.random() * 6 + 1);
+        int roll = roll1 + roll2;
+
+        return roll;
+    }
+
+    public static boolean rollforPoint(int point) {
+//        boolean gameIsOn = true;
+////        boolean result = true;
+//        while (gameIsOn == true) {
+//            int firstDice = (int) (Math.random() * 6 + 1); //6 sides
+//            int secondDice = (int) (Math.random() * 6 + 1);
+//            int Roll = firstDice + secondDice;
+//            if (Roll == 7) {
+////                result = false;
+//                gameIsOn = false;
+//
+//            } else if (Roll == point) {
+////                result = true;
+//                gameIsOn = false;
+//
+//            } else {
+//                System.out.println("You rolled " + Roll + " and so game continues");
+//
+//            }
+//        }
+        int firstRoll = point;
+        int latestRoll = point;
+
+        do {
+            System.out.println("You have " + latestRoll + " point ");
+            latestRoll = rollDice();
+        }
+        while (latestRoll != 7 && latestRoll != firstRoll);
+
+        System.out.println("You rolled " + latestRoll + " so you lose ");
+
+        return true;
+
+    }
 }
+
+
